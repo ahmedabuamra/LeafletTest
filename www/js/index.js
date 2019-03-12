a51a3f8785224373c15e900f4b4b00798d42cc97
ahmedabuamra <ah.abuamra@gmail.com>
2019-03-13 00:41:06 +0200

    calculating diffrence between device and specific clicked location

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,14 @@
 // load the map
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
+// now add the click event detector to the map
+mymap.on('click', onMapClick);
+
+// stores device location
+var devLocation = null;
+this.getLocation();
+console.log(devLocation);
+
 
 //load the tiles
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -12,6 +20,8 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 }).addTo(mymap);
 
 
+
+
 //create an event detector to wait for the user's click event and then use the popup to show them where they clicked
 function onMapClick(e) {
     // create a custom popup
@@ -20,31 +30,57 @@ function onMapClick(e) {
     popup.setLatLng(e.latlng)
         .setContent("You clicked the map at " + e.latlng.toString())
         .openOn(mymap);
+
+
+    if(devLocation){
+        var d = calculateDistance(e.latlng.lat, e.latlng.lng, devLocation.latitude, devLocation.longitude, "K");
+        console.log("Distance from you: " + d + "KM")
+    }
 }
 
-// now add the click event detector to the map
-mymap.on('click', onMapClick);
+
 
 
 
 
 function getLocation() {
     // getPosition is the function that should be called once the position has been found
-    navigator.geolocation.getCurrentPosition(getPosition);
+    return navigator.geolocation.getCurrentPosition(getPosition);
 }
 
-
-this.getLocation();
 
 
 function getPosition(position) {
     // the system automatically gives you the position variable, and you can
     // get the coordinates lat and lng from there and
     // then modify a DIV to show the results to the user
+    devLocation = position.coords;
     console.log("Position", position.coords);
 }
 
 
+
+
+
+function calculateDistance(lat1, lon1, lat2, lon2, unit) {
+    // first convert all the degree values into radians
+    // 1 radian is the angle created in a circle when the arc is equal length to the radius
+    var radlat1 = Math.PI * lat1 / 180
+    var radlat2 = Math.PI * lat2 / 180
+
+    // find the difference in longitude and convert to radians
+    var theta = lon1 - lon2
+    var radtheta = Math.PI * theta / 180
+    // then calculate the distance
+    var subAngle = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    subAngle = Math.acos(subAngle) // this is a value in radians so need to convert to km
+    subAngle = subAngle * 180 / Math.PI // distance * 2 * pi /360 OR distance * pi/180
+    
+    var dist = (subAngle / 360) * 2 * Math.PI * 3956; // angle/360 * 2 * pi * radius – calculate arc length
+    if (unit == "K") { dist = dist * 1.609344 }
+    if (unit == "N") { dist = dist * 0.8684 }
+    return dist
+}
 
 
 
